c78aef4a839b6848b8efb9a591bd2c9384135a57
vineet mishra <158786874+vineet9452@users.noreply.github.com>
2025-03-30 17:07:26 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,44 +1,3 @@
-// const nextConfig = {
-//   images: {
-//     domains: ["localhost", "127.0.0.1"],
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "3000",
-//         pathname: "/api/products/product-photo/**",
-//       },
-//     ],
-//     unoptimized: true,
-//   },
-//   reactStrictMode: false, // ✅ Strict Mode Disable करें
-// };
-
-// export default nextConfig;
-
-// const nextConfig = {
-//   images: {
-//     domains: ["localhost", "127.0.0.1"],
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "3000",
-//         pathname: "/api/products/product-photo/**",
-//       },
-//     ],
-//     unoptimized: true,
-//   },
-//   reactStrictMode: false, // ✅ Strict Mode Disable करें
-
-//   // ✅ Production Build में console.log() हटाएं
-//   compiler: {
-//     removeConsole: process.env.NODE_ENV === "production",
-//   },
-// };
-
-// export default nextConfig;
-
 const nextConfig = {
   images: {
     domains: ["localhost", "127.0.0.1"],
@@ -53,11 +12,4 @@ const nextConfig = {
     unoptimized: true,
   },
   reactStrictMode: false, // ✅ Strict Mode Disable करें
-
-  // ✅ Production Build में console.log() हटाएं
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
 };
-
-export default nextConfig;
